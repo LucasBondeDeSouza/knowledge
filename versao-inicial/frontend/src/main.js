db01f6b12e9fc8ba409836c4ca350b5d43b632c7
LucasBondeDeSouza <111398229+LucasBondeDeSouza@users.noreply.github.com>
2023-09-26 13:55:33 -0300

    atualização

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORÁRIO!
+require('axios').defaults.headers.common['Authorization'] = "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NiwibmFtZSI6IkpvYW8iLCJlbWFpbCI6ImpvYW9AeWFob28uY29tIiwiYWRtaW4iOmZhbHNlLCJpYXQiOjE2OTU3NDYyMTgsImV4cCI6MTY5NjAwNTQxOH0.-1fcgIQIVyhf6tFfNhCYfmtnaN1oFJCtkoqacEgoqCE"
+
 new Vue({
   store,
   router,
